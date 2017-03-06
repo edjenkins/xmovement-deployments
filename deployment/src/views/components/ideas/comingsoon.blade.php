0f340f5110a694282f39260a53e5cb1020333191
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2017-03-06 15:27:33 +0000

    Added blog and comingsoon blade

---
+++ deployment/src/views/components/ideas/comingsoon.blade.php
@@ -0,0 +1,7 @@
+<div class="container coming-soon-container">
+
+	<img src="https://xmovement.s3.amazonaws.com/assets/c4d/images/headers/c4d-mini-robot.svg" alt="Mini Robot">
+	<h1>Opening Soon</h1>
+	<h5>Please come back 14th March to start adding your ideas and supporting other people's!</h5>
+
+</div>
---
+++ deployment/src/views/pages/blog.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app', ['bodyclasses' => 'transparent medium-grey'])
+
+@section('content')
+    <div class="container-fluid hero-container shallow" id="terms-hero-container" style="background-image:url('{{ getenv('APP_TERMS_HEADER_IMG') }}')">
+        <div class="black-overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="text-container">
+                        <h1>Blog</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+	<div class="container">
+
+		<div class="row floating-page-row">
+
+			<div class="col-md-12">
+
+				<div class="floating-page-tile">
+					<h3>
+						The Hackathon (Create4Dementia)
+					</h3>
+
+					<p>Create4Dementia was an event hosted by Open Lab and sponsored by funds from the EPSRC and NIHR Mental Health Network. In May 2015, we invited teams of designers, coders, hackers, makers and researchers to come to Newcastle-upon-Tyne to Create-4-Dementia. Our aim was to bring people together to share ideas, experiences and concepts as well as build digital prototypes that break-down, problematize and otherwise explore the digital design space around dementia and dementia care.</p>
+
+					<p>Launchspot is inspired by the creative ways in which team members came together in this event to propose exciting solutions to complex social issues in dementia. If you check the inspiration phase, you’ll see where the team has added some of the ideas from Create4Dementia. And check out the original site here, where you’ll get a blow by blow account of how the event ran: <a target="_blank" href="http://www.create4dementia.com/">http://www.create4dementia.com/</a></p>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+@endsection
