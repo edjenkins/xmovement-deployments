88c18bafa4fdabcd292c3bace90d246031ae1a99
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2017-03-07 00:21:54 +0000

    Updated about styles

--- deployment/src/views/pages/about.blade.php
+++ deployment/src/views/pages/about.blade.php
@@ -1,152 +1,188 @@
-@extends('layouts.app', ['bodyclasses' => 'transparent medium-grey'])
+@extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid hero-container" id="about-hero-container" style="background-image:url('{{ getenv('APP_ABOUT_HEADER_IMG') }}')">
-        <div class="black-overlay"></div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="text-container">
-                        <h1>{{ trans('about.tagline') }}</h1>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 
-	<div class="container">
+	<div class="custom-about-page">
 
-		<div class="row about-summary-row">
-
-			<div class="col-sm-4">
-				<div class="about-summary-tile animated fadeInUp">
-					<img src="{{ asset(env('S3_URL') . '/icons/support.svg') }}" />
-					<h3>
-						{{ trans('about.summary-first-title') }}
-					</h3>
-
-					<p>
-						{{ trans('about.summary-first-description') }}
-					</p>
-				</div>
-			</div>
-			<div class="col-sm-4">
-				<div class="about-summary-tile animated fadeInUp">
-					<img src="{{ asset(env('S3_URL') . '/icons/design.svg') }}" />
-					<h3>
-						{{ trans('about.summary-second-title') }}
-					</h3>
-
-					<p>
-						{{ trans('about.summary-second-description') }}
-					</p>
-				</div>
-			</div>
-			<div class="col-sm-4">
-				<div class="about-summary-tile animated fadeInUp">
-					<img src="{{ asset(env('S3_URL') . '/icons/propose.svg') }}" />
-					<h3>
-						{{ trans('about.summary-third-title') }}
-					</h3>
-
-					<p>
-						{{ trans('about.summary-third-description') }}
-					</p>
-				</div>
-			</div>
-
+		<div class="vertical-timeline-header">
+			How it Works
 		</div>
 
-		<div class="row about-phase-row">
+		<div class="vertical-timeline-bar"></div>
 
-			<div class="col-md-12">
+		<div class="container">
 
-				@if (DynamicConfig::fetchConfig('INSPIRATION_MODE_ENABLED', false))
+			<!-- First Row -->
 
-					<div class="about-phase-tile animated fadeIn">
+			<div class="about-phase-row animated fadeIn">
 
-						<h3>
-							{{ trans('about.inspiration-phase-title') }}
+				<div class="calendar-tile">
+					<h5>March</h5>
+					<h3>27</h3>
+				</div>
+
+				<div class="col-md-6">
+
+					<div class="about-phase-row-content text-content left-hand-side">
+
+						<h3 class="phase-title">
+							{{ trans('about.inspire-phase-title') }}
 						</h3>
 
-						<p>
-							{{ trans('about.inspiration-phase-description') }}
+						<p class="phase-description">
+							{{ trans('about.inspire-phase-description') }}
 						</p>
 
-						<br />
+						<div class="clearfloat"></div>
 
-						<p>
-							{{ trans('about.inspiration-phase-detail') }}
+						<p class="phase-detail">
+							{{ trans('about.inspire-phase-detail') }}
 						</p>
+
+						<p class="phase-read-more" onClick="$(this).parent().toggleClass('detail-visible')">
+							<span class="read-more-text">Read more</span>
+							<span class="read-less-text">Read less</span>
+						</p>
+
 					</div>
 
-				@endif
-
-				<div class="about-phase-tile animated fadeIn">
-
-					<div class="phase-step-indicator">
-						1
-					</div>
-
-					<h3>
-						{{ trans('about.idea-phase-title') }}
-					</h3>
-
-					<p>
-						{{ trans('about.idea-phase-description') }}
-					</p>
-
-					<br />
-
-					<p>
-						{{ trans('about.idea-phase-detail') }}
-					</p>
 				</div>
 
-				<div class="about-phase-tile animated fadeIn">
+				<div class="col-md-6">
 
-					<div class="phase-step-indicator">
-						2
+					<div class="about-phase-row-content right-hand-side">
+
+						<img src="{{ asset(env('S3_URL') . '/images/about/inspiration.svg') }}" class="illustration">
+
 					</div>
 
-					<h3>
-						{{ trans('about.design-phase-title') }}
-					</h3>
-
-					<p>
-						{{ trans('about.design-phase-description') }}
-					</p>
-
-					<br />
-
-					<p>
-						{{ trans('about.design-phase-detail') }}
-					</p>
 				</div>
 
-				<div class="about-phase-tile animated fadeIn">
-
-					<div class="phase-step-indicator">
-						3
-					</div>
-
-					<h3>
-						{{ trans('about.proposal-phase-title') }}
-					</h3>
-
-					<p>
-						{{ trans('about.proposal-phase-description') }}
-					</p>
-
-					<br />
-
-					<p>
-						{{ trans('about.proposal-phase-detail') }}
-					</p>
-				</div>
+				<div class="clearfloat"></div>
 
 			</div>
+
+
+			<!-- Second Row -->
+
+			<div class="about-phase-row animated fadeIn">
+
+				<div class="calendar-tile">
+					<h5>April</h5>
+					<h3>10</h3>
+				</div>
+
+				<div class="col-sm-12 col-md-6 col-md-push-6">
+
+					<div class="about-phase-row-content text-content right-hand-side text-left">
+
+						<h3 class="phase-title">
+							{{ trans('about.design-phase-title') }}
+						</h3>
+
+						<p class="phase-description">
+							{{ trans('about.design-phase-description') }}
+						</p>
+
+						<div class="clearfloat"></div>
+
+						<p class="phase-detail">
+							{{ trans('about.design-phase-detail') }}
+						</p>
+
+						<p class="phase-read-more" onClick="$(this).parent().toggleClass('detail-visible')">
+							<span class="read-more-text">Read more</span>
+							<span class="read-less-text">Read less</span>
+						</p>
+
+					</div>
+
+				</div>
+
+				<div class="col-sm-12 col-md-6 col-md-pull-6">
+
+					<div class="about-phase-row-content left-hand-side">
+
+						<img src="{{ asset(env('S3_URL') . '/images/about/design.svg') }}" class="illustration">
+
+					</div>
+
+				</div>
+
+				<div class="clearfloat"></div>
+
+			</div>
+
+			<!-- Third Row -->
+
+			<div class="about-phase-row animated fadeIn">
+
+				<div class="calendar-tile">
+					<h5>April</h5>
+					<h3>17</h3>
+				</div>
+
+				<div class="col-md-6">
+
+					<div class="about-phase-row-content text-content left-hand-side">
+
+						<h3 class="phase-title">
+							{{ trans('about.build-phase-title') }}
+						</h3>
+
+						<p class="phase-description">
+							{{ trans('about.build-phase-description') }}
+						</p>
+
+						<div class="clearfloat"></div>
+
+						<p class="phase-detail">
+							{{ trans('about.build-phase-detail') }}
+						</p>
+
+						<p class="phase-read-more" onClick="$(this).parent().toggleClass('detail-visible')">
+							<span class="read-more-text">Read more</span>
+							<span class="read-less-text">Read less</span>
+						</p>
+
+					</div>
+
+				</div>
+
+				<div class="col-md-6">
+
+					<div class="about-phase-row-content right-hand-side">
+
+						<img src="{{ asset(env('S3_URL') . '/images/headers/c4d-home.svg') }}" class="illustration">
+
+					</div>
+
+				</div>
+
+				<div class="clearfloat"></div>
+
+			</div>
+
+			<div class="about-phase-row">
+
+				<div class="col-md-12">
+
+					<div class="about-phase-row-content text-center">
+
+						<div class="about-launch-icon">
+							<img src="{{ asset(env('S3_URL') . '/icons/propose.svg') }}" />
+						</div>
+
+					</div>
+
+				</div>
+
+				<div class="clearfloat"></div>
+
+			</div>
+
 		</div>
 
 	</div>
+
 @endsection
--- deployment/src/views/pages/blog.blade.php
+++ deployment/src/views/pages/blog.blade.php
@@ -27,7 +27,7 @@
 
 					<p>Create4Dementia was an event hosted by Open Lab and sponsored by funds from the EPSRC and NIHR Mental Health Network. In May 2015, we invited teams of designers, coders, hackers, makers and researchers to come to Newcastle-upon-Tyne to Create-4-Dementia. Our aim was to bring people together to share ideas, experiences and concepts as well as build digital prototypes that break-down, problematize and otherwise explore the digital design space around dementia and dementia care.</p>
 
-					<p>Launchspot is inspired by the creative ways in which team members came together in this event to propose exciting solutions to complex social issues in dementia. If you check the inspiration phase, you’ll see where the team has added some of the ideas from Create4Dementia. And check out the original site here, where you’ll get a blow by blow account of how the event ran: <a target="_blank" href="http://www.create4dementia.com/">http://www.create4dementia.com/</a></p>
+					<p>Launchspot is inspired by the creative ways in which team members came together in this event to propose exciting solutions to complex social issues in dementia. If you check the inspiration phase, you’ll see where the team has added some of the ideas from Create4Dementia. And check out the original site here, where you’ll get a blow by blow account of how the event ran: <a target="_blank" href="http://create4dementia.com">http://create4dementia.com</a></p>
 
 				</div>
 
