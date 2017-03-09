124775dedd581691df987147f3fb7c7393246385
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2017-03-09 15:12:07 +0000

    Wrapped more trans strings

--- deployment/src/views/components/ideas/comingsoon.blade.php
+++ deployment/src/views/components/ideas/comingsoon.blade.php
@@ -1,6 +1,6 @@
 <div class="container coming-soon-container">
 
 	<img src="https://xmovement.s3.amazonaws.com/assets/c4d/images/headers/c4d-mini-robot.svg" alt="Mini Robot">
-	<h5>This phase isn’t open yet – it’s due to begin on March 27th so make sure you check in then. We’ll send you an email reminder, but for now, why not browse the inspiration page and take some time to mull over your ideas?</h5>
+	<h5>{{ trans('create4dementia_comingsoon.message') }}</h5>
 
 </div>
