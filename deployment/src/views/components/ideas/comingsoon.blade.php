39e3243ba4aa41e57d36cf86fdccd5c6cfadc865
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2017-03-21 16:34:14 +0000

    Added registration link to comingsoon blade

--- deployment/src/views/components/ideas/comingsoon.blade.php
+++ deployment/src/views/components/ideas/comingsoon.blade.php
@@ -3,4 +3,10 @@
 	<img src="https://xmovement.s3.amazonaws.com/assets/c4d/images/headers/c4d-mini-robot.svg" alt="Mini Robot">
 	<h5>{{ trans('create4dementia_comingsoon.message') }}</h5>
 
+	@if (Auth::guest())
+		<a target="_self" href="{{ action('Auth\AuthController@showRegistrationForm') }}">
+			<button style="background-color:white;border-radius:6px;border:none;margin:20px auto;padding:10px 25px;" type="button" name="button">Notify Me</button>
+		</a>
+	@endif
+
 </div>
