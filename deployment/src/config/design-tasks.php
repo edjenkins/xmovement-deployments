83370449d826113d32410777cbe21598668a429f
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2017-03-27 17:58:45 +0100

    Updated default design tasks

--- deployment/src/config/design-tasks.php
+++ deployment/src/config/design-tasks.php
@@ -2,21 +2,18 @@
 
 return array(
 
-    'polls' => [
-        [
-	        'name' => 'Audience',
-	        'description' => 'Who will your audience or userbase be? Who will your idea help?'
-        ],
-    ],
-
     'discussions' => [
         [
-	        'name' => 'Benefits',
-	        'description' => 'What benefits do you see of those who will engage or use your idea?'
+            'name' => 'Should this be an app or website?',
+            'description' => 'Do you think this idea will be better suited as a mobile application or a website?'
         ],
         [
-	        'name' => 'Promotion',
-	        'description' => 'Do you have any ideas about how we might spread the word about the technology, and increase its use?'
+            'name' => 'Who will most likely use this technology?',
+            'description' => 'Who do you think will most likely use this app/website, for example carers or people with dementia.'
+        ],
+        [
+            'name' => 'Do you see any potential issues with this idea?',
+            'description' => 'Are there any ethical issues that need to be considered or other limitations to the idea?'
         ],
     ]
 
