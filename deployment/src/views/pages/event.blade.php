94c525d5dbd3809d13a94941fb5c188bf8a90692
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2017-05-11 21:16:24 +0100

    Updated event page

--- deployment/src/views/pages/event.blade.php
+++ deployment/src/views/pages/event.blade.php
@@ -12,16 +12,85 @@
 
 		<div class="row">
 
-			<div class="col-md-8">
+			<div class="col-md-12">
 
 				<br>
 
 				<div class="floating-page-tile">
-					<h3>
-						Title
-					</h3>
 
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<p>Have you ever wanted to make a difference to your community? Well now you can. Join us on xth June 2017 when local partners are coming together with Newcastle University’s Open Lab to create a new and exciting one-day event where you are invited to show how you would like to see Stanley move forward!</p>
+
+					<p>We will provide materials, tools, lunch, and assistance from Newcastle University designers and experts to help you demonstrate your ideas. All you need to bring is ideas and visions for your local community. And it is all for free including the lunch!</p>
+
+					<a class="btn btn-primary" href="https://www.eventbrite.co.uk" target="_self">Sign Up Now</a>
+
+					<p>Funding will be available to develop the most promising ideas from the event further and bring them to life.</p>
+
+					<p>This is your opportunity to make a real difference to where you live and we look forward to seeing you at the PACT House, 39 Front Street, DH90ST on xth June 2017.</p>
+
+				</div>
+
+				<br>
+
+				<div class="floating-page-tile">
+
+					<h3><span hidden>The Event || </span>What will happen at the event?</h3>
+
+					<p>Participants, working in groups, will be assisted by designers, skilled in the application of digital technologies to develop and test ideas quickly through various methods of prototyping. You don’t need any particular skills, just a willingness to engage with others and share your thinking.</p>
+
+					<p>The event will focus on doing things rather than just talking about them so we can all learn more about what is needed in Stanley.</p>
+
+				</div>
+
+				<br>
+
+				<div class="floating-page-tile">
+
+					<h3><span hidden>The Topic || </span>What is the focus of the event?</h3>
+
+					<p>We are interested in helping to develop all ideas and in particular how to:</p>
+
+					<ul>
+						<li>Improve job opportunities in Stanley.</li>
+						<li>Attract more visitors to Stanley.</li>
+						<li>Make Stanley more attractive.</li>
+						<li>Improve living in Stanley through up-skilling, training and apprenticeships.</li>
+					</ul>
+
+				</div>
+
+				<br>
+
+				<div class="floating-page-tile">
+
+					<h3><span hidden>For Who || </span>Why should you participate?</h3>
+
+					<p>If you care about the future of Stanley, this event is for you! Whether you live, work, or just like to spend time in Stanley, we believe your experiences are valuable and your views important to the future of Stanley.</p>
+
+					<p>If you have any creative skills to bring to the event that is great, but absolutely no prior skills or knowledge are required to take part.</p>
+
+					<p><strong>What to bring</strong></p>
+					<p>You will not have to bring anything except your ideas and a passion for the future of Stanley. But if you can and want to, you are very welcome to bring a laptop, smart phone, sewing machine, drills, soldering iron, chisels or whatever tools you prefer to be creative with. We want to keep focus on your ideas so we welcome you to use any tools and skills you already have and feel comfortable with. But don't worry if you do not have any tools to bring or do not feel you have the skills you need to demonstrate your idea. We will be ready to help you with this!</p>
+
+				</div>
+
+				<br>
+
+				<div class="floating-page-tile">
+
+					<h3><span hidden>The venue || </span>Where will this happen?</h3>
+
+					<p>The PACT House on the Front Street of Stanley has been so kind to make their cosy and centrally located building available for the whole day of the event. This building has direct access from street level and provides a welcoming and relaxed atmosphere where everyone can feel welcome.</p>
+
+					<p>PACT house is an exciting project seeking to create a one stop shop for local people, to access support and advice in a relaxed café environment.</p>
+
+					<p>[I would like these two last blurbs on the website somewhere, but I am not entirely sure they should be on the event page. Perhaps that will be too much text for one page. What do you think? Is there anywhere else it would fit? Perhaps the contact page or an about the project page?]</p>
+
+					<p><strong>Who are we?</strong></p>
+					<p>We are a group of local stakeholders including the Stanley Area Action Partnership, Karbon Homes (formerly Derwentside Homes), Smart Stanley, and Durham County Council who are interested in engaging a wider section of the local community in how they would like to see Stanley move forward and face some of the biggest challenges to our town. For this purpose we are collaborating with researchers from Newcastle University's Open Lab who have advised us on how to try out new and exciting ways to engage with the local community.</p>
+
+					<p><strong>And why are we doing this?</strong></p>
+					<p>This event is inspired by computer hackathons. These can take many forms, but the term generally refers to events where software developers meet to work intensely for a limited amount of time on a pre-set challenge or to develop ideas to address a specific topic in collaboration. What we want to take from this is a focus on “doing” rather than “talking” and to test ideas out quickly through various methods of prototyping. Researchers from Newcastle University will provide technical assistance and tools to participants on how to design and prototype their ideas; but it is important for us to stress that we consider everyone to be an expert in whatever they have experience with, which will bring something valuable to the process. This process might not necessarily result in perfect solutions, but we will almost certainly learn a lot about what the perfect solution (or the real problem) might actually look like.</p>
 
 			</div>
 
