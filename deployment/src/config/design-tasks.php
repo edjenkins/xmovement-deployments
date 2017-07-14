0fb0a2757f0acd7e81f5c1bc64c30d491d5e3acf
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2017-07-14 11:18:15 +0100

    Added pre populated design tasks

---
+++ deployment/src/config/design-tasks.php
@@ -0,0 +1,23 @@
+<?php
+
+return array(
+
+  'polls' => [
+    [
+      'name' => 'Speakers',
+      'description' => 'Who would you like to hear speak in the session and what would you like them to cover – suggest and vote below.',
+    ],
+    [
+      'name' => 'Sub-topics',
+      'description' => 'What themes, issues, questions, opportunities would you like to explore in the session – suggest and vote below.',
+    ],
+  ],
+
+  'discussions' => [
+    [
+      'name' => 'Format',
+      'description' => 'How would you like the session to run?',
+    ],
+  ],
+
+);
